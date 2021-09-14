e84dca2126c35ebb1e9752d9352bd96c39dae098
Anih Victor <anihvictor57@gmail.com>
2021-09-14 02:41:33 +0100

    changes for db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Schema::defaultStringLength(191);
+        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 
     /**
